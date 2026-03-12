6107638fc1ff07c6ff3a578df05e328df8f21b82
caveiramica14 <micaeltiago2006@gmail.com>
2026-03-12 17:28:30 +0000

    3.3 passo 5 e 6

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -20,7 +20,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-     if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+     if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
@@ -37,6 +37,8 @@ public class PlayerController : MonoBehaviour
         {
             gameOver =true;
             Debug.Log("Game Over!!");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
